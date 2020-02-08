8dcf694bf89da5603d752bce173084d81a89bf00
Jason Junio <jjjunio@gmail.com>
2020-02-08 03:56:46 +0000

    Assignment - Initial Commit Javascripting

---
+++ looping-through-arrays.js
@@ -0,0 +1,12 @@
+
+const pets = ['cat', 'dog', 'rat']
+
+let total = 0
+const limit = 3
+
+for (let i = 0; i < limit; i++) {
+  pets[i] = pets[i] + 's'
+}
+
+console.log(pets)
+
---
+++ number-to-string.js
@@ -0,0 +1,6 @@
+
+const n = 128
+console.log(n.toString())
+
+
+
